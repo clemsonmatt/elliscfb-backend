e6ed797fba9905a58e33f7bd184a7e159749c530
Matt Ellis <mellis0292@gmail.com>
2023-03-21 15:36:18 -0400

    Include teams with conference data

--- app/controllers/conferences_controller.rb
+++ app/controllers/conferences_controller.rb
@@ -1,2 +1,7 @@
 class ConferencesController < ApplicationController
+  def index
+    conferences = Conference.all.order(name: :asc)
+
+    render json: conferences.to_json(include: :teams)
+  end
 end
--- app/models/conference.rb
+++ app/models/conference.rb
@@ -1,2 +1,3 @@
 class Conference < ApplicationRecord
+  has_many :teams, -> { order(name: :asc) }
 end
