1d2a4c8d6cec76540a5bad30adaaf72006dacc74
Matt Ellis <mellis0292@gmail.com>
2023-03-24 15:24:01 -0400

    Update CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"
+    if Rails.env.development?
+      origins "http://localhost:5173"
+    else
+      origins "https://elliscfb.netlify.app"
+    end
 
     resource "*",
       headers: :any,
