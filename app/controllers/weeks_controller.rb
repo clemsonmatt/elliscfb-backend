8c9169f2fc99a2adfbe4ff443e285ddca02f89a8
Matt Ellis <mellis0292@gmail.com>
2023-09-05 14:22:42 -0400

    Show weeks pickem is available

--- app/controllers/weeks_controller.rb
+++ app/controllers/weeks_controller.rb
@@ -8,4 +8,16 @@ class WeeksController < ApplicationController
 
     render json: { weeks: weeks, current_week: current_week }
   end
+
+  def pickem_available
+    season = Season.where(active: true).first
+    weeks = Week.where(season: season)
+
+    current_week = Week.current_week.first
+    current_week = weeks.first if current_week.nil?
+
+    weeks = Week.where(season: season).where("end_date <= ?", current_week.end_date)
+
+    render json: { weeks: weeks, current_week: current_week }
+  end
 end
